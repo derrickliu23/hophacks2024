99a763bdbf309443f5786cca80f0fc55fd688f2f
Dylan <your.dylanpark09@gmail.com>
2024-09-14 17:45:59 -0400

    no more red

--- frontend/src/components/PatientDashboard.js
+++ frontend/src/components/PatientDashboard.js
@@ -87,7 +87,7 @@ const PatientCard = ({ patient, onClick }) => (
     {patient.isUrgent && <UrgentIndicator />}
     <div style={{
       ...styles.cardHeader,
-      backgroundColor: patient.isUrgent ? '#FF0000' : styles.cardHeader.backgroundColor, // Red background for urgent cases
+      backgroundColor: patient.isUrgent ? '#89CFF0' : styles.cardHeader.backgroundColor, // Red background for urgent cases
       color: patient.isUrgent ? 'white' : styles.cardHeader.color, // Ensure text is white on red background
     }}>
       {patient.name}
@@ -197,7 +197,7 @@ const PatientDetailedInfo = ({ patient }) => (
       boxSizing: 'border-box',
     },
     header: {
-      backgroundColor: '#3498db',
+      backgroundColor: '#89CFF0',
       width: '100%',
       padding: '20px',
       display: 'flex',
@@ -250,7 +250,7 @@ const PatientDetailedInfo = ({ patient }) => (
     cardHeader: {
       fontSize: '1.2rem',
       fontWeight: 'bold',
-      backgroundColor: '#3498db',
+      backgroundColor: '#89CFF0',
       color: 'white',
       padding: '10px',
       borderRadius: '8px',
